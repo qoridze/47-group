f9b3218e173e6dc7dd0f273c98b5312ebc872044
qoridze <qoridzenikoloz66@gmail.com>
2025-04-28 20:05:49 +0400

    second commit

---
+++ level 60/classwork/classwork.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/55b95c76e08bd5eef100001e/train/python
+    # adak_count = n // 2
+    # result = "adak " * adak_count
+    # if n % 2 != 0:
+    #     result += "anane"
+    # return result.strip()
+# https://www.codewars.com/kata/67d4554cd94fdfdab4239cfa/train/python
+def is_planet_mnemonic_correct(solar_system, mnemonic):
+    filtered_solar_system = [planet for planet in solar_system if planet != "Asteroid"]
+    
+    mnemonic_initials = [word[0] for word in mnemonic.split()]
+
+    return [planet[0] for planet in filtered_solar_system] == mnemonic_initials
+# https://www.codewars.com/kata/59656c69253c365e58000046/train/python
---
+++ level 67/classwork/classwork.py
@@ -0,0 +1,2 @@
+average = lambda a, b, c: (a + b + c) / 3
+print(average(3, 6, 9))
