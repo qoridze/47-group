dfc377465ed5a7dd35d9fccd962bf8861a6b9923
qoridze <qoridzenikoloz66@gmail.com>
2025-05-12 21:04:17 +0400

    second commit

---
+++ level 72/classwork/script.js
@@ -0,0 +1,20 @@
+console.log("Hello, World!");
+
+name = "nikoloz" ;
+name = "daviti" ;
+console.log(name);
+
+const array = lomi
+
+console.log(array);
+
+var age = 16
+
+var age = 14
+
+const body = document.getElementById("body")
+console.log(body);
+
+function bgCHance(){
+    Body.style.background = "skyblue"
+}
